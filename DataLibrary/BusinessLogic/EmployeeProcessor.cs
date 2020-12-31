ea8fb686af391d1e9b568a32dc58d6f5addcd6a5
zdzislaw dyrman <zdzislawdyrman1@gmail.com>
2020-12-31 18:00:20 +0000

    First Commit work in progress

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,46 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class EmployeeProcessor
+    {
+        public static int CreateEmployee(int employeeId, string firstName, 
+            string lastName, string emailAddress)
+        {
+            EmployeeModel data = new EmployeeModel
+            {
+                EmployeeId = employeeId,
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress
+            };
+
+            string sql = @"insert into dbo.EmployeesTable(EmployeeId, FirstName, LastName, EmailAddress)
+                            values (@EmployeeId, @FirstName, @LastName, @EmailAddress);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = @"select Id, EmployeeId, FirstName, LastName, EmailAddress
+                            from dbo.EmployeesTable;";
+
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+
+        public static void DeleteEmployee(int empNum)
+        {
+            string sql = @"DELETE from dbo.EmployeesTable where EmployeeId = " +empNum;
+
+            SqlDataAccess.DeleteData(sql);
+        }
+
+    }
+}
---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLibrary.DataAccess
+{
+    public static class SqlDataAccess
+    {
+        public static string GetConnectionString(string connectionName = "MVCEmployeeDB")
+            // connectionName has to mach the name from Web.config and conncetionName
+            // I had a different name there that is why i had a problem !!!
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+        public static void DeleteData(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                cnn.Execute(sql);
+            }
+        }
+    }
+}
